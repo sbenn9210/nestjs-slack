48a84f04f1968205e3bc48b68b7166d11bda11e1
Shawn Benny <shawn.benny@umbrage.com>
2022-12-06 13:17:17 -0600

    Working version of typeorm

---
+++ api/ormconfig-migration.ts
@@ -0,0 +1,2 @@
+import config from 'ormconfig';
+export = config;
--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,23 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import config, { typeOrmAsyncConfig } from 'ormconfig';
 import { DataSource } from 'typeorm';
-import { User } from './users/users.entity';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: [User],
-      synchronize: true,
-    }),
+    TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
     UsersModule,
   ],
 })
