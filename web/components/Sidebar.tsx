f1839e1d496da2a420a2c8c06627b552830b128c
Shawn Benny <shawn.benny@umbrage.com>
2022-12-13 13:54:01 -0600

    Add chat rooms and messaging functionality

--- web/components/Sidebar.tsx
+++ web/components/Sidebar.tsx
@@ -5,20 +5,20 @@ import {
   FolderIcon,
   HomeIcon,
   InboxIcon,
-  UsersIcon,
-} from '@heroicons/react/24/outline';
+  UsersIcon
+} from "@heroicons/react/24/outline";
 
 const navigation = [
-  { name: 'Dashboard', icon: HomeIcon, href: '#', current: true },
-  { name: 'Team', icon: UsersIcon, href: '#', count: 3, current: false },
-  { name: 'Projects', icon: FolderIcon, href: '#', count: 4, current: false },
-  { name: 'Calendar', icon: CalendarIcon, href: '#', current: false },
-  { name: 'Documents', icon: InboxIcon, href: '#', current: false },
-  { name: 'Reports', icon: ChartBarIcon, href: '#', count: 12, current: false },
+  { name: "Dashboard", icon: HomeIcon, href: "#", current: true },
+  { name: "Team", icon: UsersIcon, href: "#", count: 3, current: false },
+  { name: "Projects", icon: FolderIcon, href: "#", count: 4, current: false },
+  { name: "Calendar", icon: CalendarIcon, href: "#", current: false },
+  { name: "Documents", icon: InboxIcon, href: "#", current: false },
+  { name: "Reports", icon: ChartBarIcon, href: "#", count: 12, current: false }
 ];
 
 function classNames(...classes: any) {
-  return classes.filter(Boolean).join(' ');
+  return classes.filter(Boolean).join(" ");
 }
 
 export default function Example() {
@@ -36,17 +36,17 @@ export default function Example() {
                 href={item.href}
                 className={classNames(
                   item.current
-                    ? 'bg-gray-900 text-white'
-                    : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                  'group flex items-center px-2 py-2 text-sm font-medium rounded-md'
+                    ? "bg-gray-900 text-white"
+                    : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                  "group flex items-center px-2 py-2 text-sm font-medium rounded-md"
                 )}
               >
                 <item.icon
                   className={classNames(
                     item.current
-                      ? 'text-gray-300'
-                      : 'text-gray-400 group-hover:text-gray-300',
-                    'mr-3 flex-shrink-0 h-6 w-6'
+                      ? "text-gray-300"
+                      : "text-gray-400 group-hover:text-gray-300",
+                    "mr-3 flex-shrink-0 h-6 w-6"
                   )}
                   aria-hidden="true"
                 />
@@ -55,9 +55,9 @@ export default function Example() {
                   <span
                     className={classNames(
                       item.current
-                        ? 'bg-gray-800'
-                        : 'bg-gray-900 group-hover:bg-gray-800',
-                      'ml-3 inline-block py-0.5 px-3 text-xs font-medium rounded-full'
+                        ? "bg-gray-800"
+                        : "bg-gray-900 group-hover:bg-gray-800",
+                      "ml-3 inline-block py-0.5 px-3 text-xs font-medium rounded-full"
                     )}
                   >
                     {item.count}
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,25 +1,59 @@
-import Search from '../components/Search';
-import Sidebar from '../components/Sidebar';
+import { useEffect, useRef } from "react";
+import Search from "../components/Search";
+import Sidebar from "../components/Sidebar";
+import MessagesContainer from "../containers/Messages";
+import RoomsContainer from "../containers/Rooms";
+import { useSockets } from "../context/socket.context";
 
 export function Index(props: any) {
+  const { socket, username, setUsername } = useSockets();
+  const usernameRef = useRef({});
+
+  function handleSetUsername() {
+    const value = usernameRef?.current?.value;
+    if (!value) {
+      return;
+    }
+
+    setUsername(value);
+
+    localStorage.setItem("username", value);
+  }
+
+  useEffect(() => {
+    if (usernameRef)
+      usernameRef.current.value = localStorage.getItem("username") || "";
+  }, []);
+
   return (
-    <div className="flex flex-col bg-gray-100 overflow-y-auto">
-      <Search />
-      <Sidebar />
+    <div>
+      {!username && (
+        <div>
+          <input placeholder="Username" ref={usernameRef} />
+          <button onClick={handleSetUsername}>START</button>
+        </div>
+      )}
+      {username && (
+        <>
+          <RoomsContainer />
+          <MessagesContainer />
+        </>
+      )}
+      <div className="flex flex-col bg-gray-100 overflow-y-auto"></div>
     </div>
   );
 }
 
-export async function getServerSideProps(context: any) {
-  let user = {};
-  const res = await fetch('http://localhost:3333/api');
-  user = await res.json();
+// export async function getServerSideProps(context: any) {
+//   let user = {};
+//   const res = await fetch("http://localhost:3333/api");
+//   user = await res.json();
 
-  return {
-    props: {
-      user,
-    },
-  };
-}
+//   return {
+//     props: {
+//       user
+//     }
+//   };
+// }
 
 export default Index;
